adbc2e8c9b213e5887565a51bd1bfaf3dba68962
fadhildimas <75319605+fadhildimas@users.noreply.github.com>
2021-10-28 15:46:00 +0700

    studi mission 9

---
+++ README.md
@@ -0,0 +1,2 @@
+# studi-kasus-1-Raharja214
+studi-kasus-1-Raharja214 created by GitHub Classroom
---
+++ login_action.php
@@ -0,0 +1,2 @@
+Welcome <?php echo $_POST["email"]; ?><br>
+Your pwd is: <?php echo $_POST["password"]; ?>
